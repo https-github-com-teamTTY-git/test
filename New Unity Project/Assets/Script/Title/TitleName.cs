d5e994905dc39374a6afc6e7a8296cd51cf52e90
田口未来 <en314ahodw@ezweb.ne.jp>
2019-11-06 20:26:39 +0900

    TitleScene追加

---
+++ New Unity Project/Assets/Script/Title/TitleName.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleName : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (this.GetComponent<Image>().fillAmount < 1)
+        {
+            this.GetComponent<Image>().fillAmount += 0.01f;
+        }
+    }
+}
---
+++ New Unity Project/Assets/Script/Title/TitleRotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleRotate : MonoBehaviour
+{
+    private Quaternion angle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        angle = this.transform.localRotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(0, 0, 0.5f);
+    }
+}
