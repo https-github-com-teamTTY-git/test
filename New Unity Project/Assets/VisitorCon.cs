a64031f6e6acddef84daaaacaedbe7596875b05d
nonaka <heid>
2019-11-06 17:41:54 +0900

    席に移動

--- New Unity Project/Assets/VisitorCon.cs
+++ New Unity Project/Assets/VisitorCon.cs
@@ -4,18 +4,34 @@ using UnityEngine;
 
 public class VisitorCon : MonoBehaviour
 {
-    CharacterController charaCon;
-    float velocity = 1.0f;
-    
+
+    private Chair chair;
     // Start is called before the first frame update
     void Start()
     {
-        charaCon = GetComponent<CharacterController>();
+        GameObject obj = GameObject.Find("Chair_1");
+        chair = obj.GetComponent<Chair>();
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        if(this.gameObject.transform.position.x < chair.gameObject.transform.position.x)
+        {
+            this.gameObject.transform.Translate(-0.05f, 0, 0);
+        }
+        if (this.gameObject.transform.position.x > chair.gameObject.transform.position.x)
+        {
+            this.gameObject.transform.Translate(0.05f, 0, 0);
+        }
+        if (this.gameObject.transform.position.z < chair.gameObject.transform.position.z)
+        {
+            this.gameObject.transform.Translate(0, 0, -0.05f);
+        }
+        if (this.gameObject.transform.position.z > chair.gameObject.transform.position.z)
+        {
+            this.gameObject.transform.Translate(0, 0, 0.05f);
+        }
+        Debug.Log(this.gameObject.transform.position);
     }
 }
