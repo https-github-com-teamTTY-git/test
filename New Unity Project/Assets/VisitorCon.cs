b42c92814a07139442522d512b5d4adfe021d8e9
nonaka <heid>
2019-11-06 15:36:22 +0900

    ステージ作成

---
+++ New Unity Project/Assets/VisitorCon.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisitorCon : MonoBehaviour
+{
+    CharacterController charaCon;
+    float velocity = 1.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        charaCon = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
