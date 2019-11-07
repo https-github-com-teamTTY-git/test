6a697a3167c8f0b54cffa282b9327be3b77ccaea
mine <hye>
2019-11-07 16:49:43 +0900

    フォント変更

---
+++ New Unity Project/Assets/Scenes/GameScene.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameScene : MonoBehaviour
+{
+    protected static int Score = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 数字で経過時間を決める
+        Invoke("ChangeScene", 10.0f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void ChangeScene()
+    {
+        SceneManager.LoadScene("ResultScene");
+    }
+
+
+}
