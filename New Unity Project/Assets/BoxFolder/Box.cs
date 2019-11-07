a8da9de3b03755fc98800752468e49b2ede4ba50
田口未来 <en314ahodw@ezweb.ne.jp>
2019-11-07 12:22:00 +0900

    スクリプトの移動と、寿司を上からスポーンさせるとこ、寿司セットを追加しました

--- New Unity Project/Assets/BoxFolder/Box.cs
+++ New Unity Project/Assets/BoxFolder/Box.cs
@@ -4,20 +4,6 @@ using UnityEngine;
 
 public class Box : MonoBehaviour
 {
-    [SerializeField]
-    int a = 0;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     // 衝突した際に親子関係を持たせる
     void OnCollisionEnter(Collision collision)
     {
---
+++ New Unity Project/Assets/Script/SushiSpone.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SushiSpone : MonoBehaviour
+{
+    SushiRundom sushiRundom = null;
+    private bool randStart;         //乱数を取るかどうかのフラグ
+    private int nextObjNum;         //次回の種別
+
+    //寿司モデルのプレハブリスト
+    private List<GameObject> sushiObjList;
+    private List<int> sushiCount;
+    private int totalCnt;
+
+    //スポーン用変数
+    [SerializeField]
+    private int spTimeSecond = default;         //スポーンする間隔（秒）
+    private int flamNum;                        //フレーム数
+    private int flamSecond = 60;                //1秒間のフレーム数
+    [SerializeField]
+    private int maxSponeCnt = default;
+
+    private void Start()
+    {
+        //Resourcesフォルダ内のprefabフォルダ内の全てのプレハブを取得
+        Object[] sushiObj = Resources.LoadAll("prefab");
+        sushiObjList = new List<GameObject>();
+        sushiCount = new List<int>();
+        foreach (GameObject obj in sushiObj)
+        {
+            sushiCount.Add(0);
+            sushiObjList.Add(obj);
+        }
+        sushiRundom = this.GetComponent<SushiRundom>();
+        randStart = false;
+        nextObjNum = sushiRundom.GetRandom(sushiObjList.Count);
+        flamNum = 0;
+        totalCnt = 0;
+    }
+
+    private void Update()
+    {
+        if (randStart)
+        {
+            //次出現させる寿司を決めておく
+            nextObjNum = sushiRundom.GetRandom(sushiObjList.Count);
+            randStart = false;
+        }
+
+        flamNum++;
+        //フレーム数が指定した間隔になればスポーンor注文させる
+        if (flamNum % (spTimeSecond * flamSecond) == 0)
+        {
+            totalCnt++;
+            if (totalCnt <= maxSponeCnt)
+            {
+                //指定オブジェクトスポーン
+                GameObject obj = Instantiate<GameObject>(sushiObjList[nextObjNum], this.transform.position, Quaternion.Euler(-90, 0, 90));
+                sushiCount[nextObjNum]++;
+                randStart = true;
+            }
+        }
+
+        // [Debug] 各種類の合計数を表示する
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            for (int j = 0; j < sushiCount.Count; j++)
+            {
+                Debug.Log(sushiCount[j]);
+            }
+        }
+    }
+
+    //次落ちてくる寿司のオブジェクトを取得
+    public GameObject GetNextSushi()
+    {
+        return sushiObjList[nextObjNum];
+    }
+}
