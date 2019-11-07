b0694c24a64db1285f18d8f8a0aa81135e3079fc
somekawa <tkg>
2019-11-07 15:06:13 +0900

    スタートボタンでゲームシーンへ移行

---
+++ New Unity Project/Assets/Script/Title/StartButtonScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButtonScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    public void OnClick()
+    {
+        // ゲームシーンへ移動
+        SceneManager.LoadScene("Scenes/GameScene");
+        Debug.Log("ゲームシーンへ!");  // ログを出力
+    }
+
+}
