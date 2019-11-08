8ae76b2b7430bf6fdadc9c3972fd4586d05ab10d
田口未来 <en314ahodw@ezweb.ne.jp>
2019-11-08 10:28:48 +0900

    寿司セットのプレハブ追加と、落ちてきた寿司が伸びるバグ直しました

--- New Unity Project/Assets/BoxFolder/Box.cs
+++ New Unity Project/Assets/BoxFolder/Box.cs
@@ -4,9 +4,44 @@ using UnityEngine;
 
 public class Box : MonoBehaviour
 {
+    private Vector3 sushiScale;
+    private Quaternion rotate;
+    private List<string> rideOnList;
+    private bool destroyFlag;
+    private GameObject parentObj;
+
+    private void Start()
+    {
+        sushiScale = transform.lossyScale;
+        rotate = transform.rotation;
+        rideOnList = new List<string>();
+        Object[] sushiObj = Resources.LoadAll("prefab");
+        foreach (GameObject obj in sushiObj)
+        {
+            rideOnList.Add(obj.name);
+        }
+        rideOnList.Add("lane");
+        rideOnList.Add("rice");
+        destroyFlag = true;
+        parentObj = GameObject.Find("DefaultRotaObj");
+    }
+
     // 衝突した際に親子関係を持たせる
     void OnCollisionEnter(Collision collision)
     {
-        transform.parent = GameObject.Find("lane").transform;
+        foreach (string name in rideOnList)
+        {
+            if(name == collision.gameObject.name)
+            {
+                destroyFlag = false;
+                transform.parent = parentObj.transform;
+                break;
+            }
+        }
+
+        if (destroyFlag)
+        {
+            Destroy(this.transform.gameObject);
+        }
     }
 }
