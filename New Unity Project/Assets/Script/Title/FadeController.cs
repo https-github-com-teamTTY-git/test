08712e154e1047e858a284b01b31dd93f549b598
somekawa <tkg>
2019-11-08 10:49:50 +0900

    fadeoutまでの流れを追加

---
+++ New Unity Project/Assets/Script/Title/FadeController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeController : MonoBehaviour
+{
+    float fadeSpeed = 0.02f;        //透明度が変わるスピードを管理
+    float red, green, blue, alfa;   //パネルの色、不透明度を管理
+
+    //public bool isFadeOut = false;  //フェードアウト処理の開始、完了を管理するフラグ
+    //public bool isFadeIn = false;   //フェードイン処理の開始、完了を管理するフラグ
+    public bool isendFadeFlg = false;        // 終わったらtrue
+
+    Image fadeImage;                //透明度を変更するパネルのイメージ
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fadeImage = GetComponent<Image>();
+        red = fadeImage.color.r;
+        green = fadeImage.color.g;
+        blue = fadeImage.color.b;
+        alfa = fadeImage.color.a;
+    }
+
+        // Update is called once per frame
+    void Update()
+    {
+        isendFadeFlg = false;
+    }
+
+    public bool StartFadeIn(){
+		alfa -= fadeSpeed;                //a)不透明度を徐々に下げる
+		SetAlpha ();                      //b)変更した不透明度パネルに反映する
+		if(alfa <= 0.0f){                    //c)完全に透明になったら処理を抜ける
+			fadeImage.enabled = false;    //d)パネルの表示をオフにする
+            isendFadeFlg = true;
+        }
+        return isendFadeFlg;
+    }
+ 
+	public bool StartFadeOut(){
+		fadeImage.enabled = true;  // a)パネルの表示をオンにする
+		alfa +=  fadeSpeed;         // b)不透明度を徐々にあげる
+        Debug.Log(alfa);
+		SetAlpha ();               // c)変更した透明度をパネルに反映する
+		if(alfa >= 1.0f){             // d)完全に不透明になったら処理を抜ける
+
+            isendFadeFlg = true;
+        }
+        return isendFadeFlg;
+    }
+
+    void SetAlpha(){
+		fadeImage.color = new Color(red, green, blue, alfa);
+	}
+}
--- New Unity Project/Assets/Script/Title/StartButtonScript.cs
+++ New Unity Project/Assets/Script/Title/StartButtonScript.cs
@@ -5,21 +5,53 @@ using UnityEngine.SceneManagement;
 
 public class StartButtonScript : MonoBehaviour
 {
+   bool isFadeOut = false;         //フェードアウト処理の開始、完了を管理するフラグ
+   bool isFadeIn = false;   //フェードイン処理の開始、完了を管理するフラグ
+
+    private FadeController fadeCtl;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        GameObject obj = GameObject.Find("Fade");   // 指定の名前のオブジェクトを探す(処理重い※update書込禁止)
+        fadeCtl = obj.GetComponent<FadeController>();
+        fadeCtl.isendFadeFlg = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-    }
-    public void OnClick()
-    {
-        // ゲームシーンへ移動
-        SceneManager.LoadScene("Scenes/GameScene");
-        Debug.Log("ゲームシーンへ!");  // ログを出力
+        if(isFadeOut)
+        {
+            // fadeOutのreturnでisendFadeFlgがtrueになったとき
+            if (fadeCtl.StartFadeOut())
+            {
+                // fadeInする
+                isFadeOut = false;
+                //isFadeIn = true;
+                fadeCtl.isendFadeFlg = false;
+                // ゲームシーンへ
+                SceneManager.LoadScene("Scenes/GameScene");
+
+            }
+        }
+
+        // fadeInのreturnでisendFadeFlgがtrueになったとき
+        // GameSceneで新しくパネルをつかってやったほうがいい
+        //if(isFadeIn)
+        //{
+        //    if (fadeCtl.StartFadeIn())
+        //    {
+        //        isFadeIn = false;
+        //        // ゲームシーンへ
+        //        SceneManager.LoadScene("Scenes/GameScene");
+        //    }
+        //}
     }
 
+    public void OnClick()
+    {
+        // fadeOutする
+        isFadeOut = true;
+    }
 }
