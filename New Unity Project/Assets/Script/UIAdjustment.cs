17412752528e4283dc71499644eabb0990b2c6ec
田口未来 <en314ahodw@ezweb.ne.jp>
2019-11-12 17:45:46 +0900

    UI調整しました

---
+++ New Unity Project/Assets/Script/UIAdjustment.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIAdjustment : MonoBehaviour
+{
+    private Vector2 screenSize;
+    private CanvasScaler scaler;
+    void Start()
+    {
+        screenSize = new Vector2(Screen.width, Screen.height);
+        Debug.Log(screenSize);
+        scaler = this.GetComponent<CanvasScaler>();
+        scaler.referenceResolution = screenSize;
+    }
+}
